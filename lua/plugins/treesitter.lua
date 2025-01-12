3565fefe362b5c057fbf6cc8f7210989fd835081
Voidbloom13 <dakota.huyghe.dev@gmail.com>
2025-01-12 03:43:37 -0500

    nvim config files

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "lua",
+                "vim",
+                "vimdoc",
+                "java",
+                "javascript",
+                "html",
+                "python"
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true }
+        })
+    end
+}
